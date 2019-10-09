97de0e95cf99ce4e5454938e0187a32b4d39073a
Neto Concon <concon.neto2@gmail.com>
2019-10-09 16:00:05 -0300

    notes in mixin

--- mixins.rb
+++ mixins.rb
@@ -4,14 +4,16 @@ mixin BMI {
   }
 }
 
-class Person with BMI {
+# create a mixin with variables and methods
+
+class Person with BMI { # => add mixin to an existing class
   Person({this.name, this.age, this.height, this.weight});
   final String name;
   final int age;
   final double height;
   final double weight;
 
-  double get bmi => calculateBMI(weight, height);
+  double get bmi => calculateBMI(weight, height); # create a computed variable that return the result of calculateBMI method
 
 }
 
@@ -19,3 +21,7 @@ void main() {
   final person = Person(name: "John", age: 40, height: 1.9, weight: 100.0);
   print(person.bmi);
 }
+
+# why mixing?
+
+# extract and reuse functionality in multiple classes
