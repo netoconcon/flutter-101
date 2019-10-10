09d44990d239d5fa3ffb89782a2bcc0d212cae31
Neto Concon <concon.neto2@gmail.com>
2019-10-10 13:00:21 -0300

    add if and else statement

---
+++ flow.dart
@@ -0,0 +1,17 @@
+void main() {
+  printOddEven(5);
+}
+
+void printOddEven( int value) {
+  if (value % 2 == 0) {
+    print('$value is Even');
+  } else {
+    print('$value is Odd');
+  }
+}
+
+   // if (condition) {
+  //  execute if true
+  // } else {
+  //   execute if false
+  // }
